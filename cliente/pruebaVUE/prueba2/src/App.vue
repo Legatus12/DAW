34fc95a1f1b92b0c07700163407d244f81737597
Legatus12 <pablo.clase.daw@gmail.com>
2022-10-26 14:10:10 +0200

    26/10/22

--- cliente/pruebaVUE/prueba2/src/App.vue
+++ cliente/pruebaVUE/prueba2/src/App.vue
@@ -1,24 +1,49 @@
-<script setup>
-import Component1 from './components/Component1.vue';
-import Component2 from './components/Component2.vue';
-
-</script>
-
 <template>
-  <div id="container">
-    <Component1 />
-    <Component2 />
+  <div>
+    <input type="text" ref="input" v-model="item">
+    <button @click="addItem">add</button>
+  </div>
+  <div>
+    filtrar =>
+    <input type="text" v-model="filter" >
+  </div>
+  <div>
+    <ul>
+      <li v-for="item in filtered">{{ item }}</li>
+    </ul>
   </div>
 </template>
 
-<style scoped>
-
-  #container{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 50px;
-    color: white;
+<script>
+  export default{
+    data(){
+      return{
+        item : "",
+        items : [],
+        filter : ""
+      }
+    },
+    methods:{
+      addItem(){
+        console.log(this.item);
+        this.items.push(this.item);
+        this.item = "";
+        this.$refs.input.focus();
+      }
+    },
+    computed:{
+      filtered(){
+        if(this.filter !== "")
+          return this.items.filter(x => x.includes(this.filter));
+        else 
+          return this.items;
+      }
+    }
   }
+</script>
 
+<style scoped>
+  li{
+    color:brown;
+  }
 </style>
