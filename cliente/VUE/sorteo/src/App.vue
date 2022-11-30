66baa0068fd1579941e67eb181beb0fbee6cdc49
Legatus12 <pablo.clase.daw@gmail.com>
2022-11-30 13:58:36 +0100

    30/11/22

--- cliente/VUE/sorteo/src/App.vue
+++ cliente/VUE/sorteo/src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <ReadFile texto="Elige un fichero" @carga="cargaFichero"/>
-  <p v-if="numeroInCorrecto">No es un número par</p>
+  <p v-if="numeroIncorrecto">No es un número par</p>
   <ul >
     <li v-for="(el,key) in fichero" :key="key"> {{ el[0] }}- {{ el[1] }} </li>
   </ul>
   
 </template>
 
-<script>
+<script setup>
 
 import ReadFile from "@/components/ReadFile.vue";
+import { ref } from "vue";
 
+const fichero = ref([]);
+const numeroIncorrecto = ref(true);
+
+function cargaFichero(fich){
+  //Pasamos la cadena de caracteres del fichero obtenido a un array
+  let auxiliar =  fich
+    .toString()
+    .trim()
+    .split('\n')
+  //Si el número de elementos no es par, no podemos continuar
+  if (auxiliar.length % 2 == 0) {
+    numeroIncorrecto.value = false;
+    //Realizamos el emparejamiento aleatorio
+    fichero.value = auxiliar
+      .sort(()=> Math.random() - 0.5)
+      .map((el,idx,arr)=>  (idx % 2 == 1) ? [el,arr[idx-1]]: null)
+      .filter( el => el !== null);
+  } else {
+    //No ha sido un número correcto
+    numeroIncorrecto.value = true;
+    fichero.value = [];
+  }
+}
+
+
+/*
 export default {
   components:{
     ReadFile,
@@ -46,5 +73,5 @@ export default {
         
   }, 
 };
-
+*/
 </script>
--- cliente/VUE/sorteo/src/components/ReadFile.vue
+++ cliente/VUE/sorteo/src/components/ReadFile.vue
@@ -1,27 +1,25 @@
 <template>
     <label >
-      {{ this.texto }} 
+      {{ texto }} 
       <input type="file" @change="loadTextFromFile">
     </label>       
 </template>
   
-<script>
-  export default {
-    name:"ReadFile",
-    props:{
-        texto:{
-            type: String,
-        },
-    },
-    methods: {
-      loadTextFromFile(ev) {
-        const file = ev.target.files[0];
-        const reader = new FileReader();
-        reader.onload = e => this.$emit("carga",e.target.result);
-        reader.readAsText(file);
-      }    
-    },
-    
-  }
-  </script>
-  <style></style>
+<script setup>
+
+const props = defineProps({
+  texto: String
+})
+
+const emits = defineEmits(["carga"])
+
+function loadTextFromFile(ev){
+  const file = ev.target.files[0];
+  const reader = new FileReader();
+  reader.onload = e => emits("carga",e.target.result);
+  reader.readAsText(file);
+}  
+
+</script>
+
+<style></style>
