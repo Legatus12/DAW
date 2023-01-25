f688d841e22511644f65df7b5de4ad8240cab33c
Legatus12 <pablo.clase.daw@gmail.com>
2023-01-25 13:56:05 +0100

    25/1/23

--- cliente/DOM2/intro/firebase.js
+++ cliente/DOM2/intro/firebase.js
@@ -16,16 +16,20 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCPT7Fu_3pwFNsAJ4NBLLvpgApvv0IoaEU",
-    authDomain: "test-ddd89.firebaseapp.com",
-    projectId: "test-ddd89",
-    storageBucket: "test-ddd89.appspot.com",
-    messagingSenderId: "698250421716",
-    appId: "1:698250421716:web:f1d58a9dd606e83f65e241",
-    measurementId: "G-FZV39SN6H4"
+  apiKey: "AIzaSyCPT7Fu_3pwFNsAJ4NBLLvpgApvv0IoaEU",
+  authDomain: "test-ddd89.firebaseapp.com",
+  projectId: "test-ddd89",
+  storageBucket: "test-ddd89.appspot.com",
+  messagingSenderId: "698250421716",
+  appId: "1:698250421716:web:f1d58a9dd606e83f65e241",
+  measurementId: "G-FZV39SN6H4"
 };
 
 //Conectamos con la base de datos
 const app = initializeApp(firebaseConfig);
 const db = getFirestore()
 //CRUD
+
+export const getDocuments = (ref) => getDocs(collection(db, ref));
+
+export const onGetDocuments = (ref, callback) => onSnapshot(collection(db, ref), callback);
--- cliente/DOM2/intro/script.js
+++ cliente/DOM2/intro/script.js
@@ -1,1 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => console.log("echo"))
+import { getDocuments, onGetDocuments } from './firebase.js';
+
+//
+
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+
+window.addEventListener('DOMContentLoaded', async() => {
+    await onGetDocuments("pets", (docs) => {
+        docs.forEach(pet => {
+            const { animal, petName } = pet.data();
+            list.innerHTML += `
+            <div>
+                <h2>${petName}</h2>
+                <h3>${animal}</h3>
+            </div>`
+        })
+    })
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const petName = form['petName'].value;
+    const animal = form['animal'].value;
+    list.innerHTML += `
+    <div>
+        <h2>${petName}</h2>
+        <h3>${animal}</h3>
+    </div>
+    `
+    form.reset();
+});
